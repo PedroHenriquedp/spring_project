730032cfa70208ab949ae72513798198f22d43e0
PedroHenriquedp <pereira.henrique@aluno.ufabc.edu.br>
2024-07-18 01:02:35 -0300

    Classe de usuários criada

---
+++ src/main/java/com/pedrohdp/project/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.pedrohdp.project.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pedrohdp.project.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+        User u = new User(1L, "Pedro", "pedrodp@gmail.com", "11968500707", "umHashAqui@");
+        return ResponseEntity.ok().body(u);
+    }
+}
